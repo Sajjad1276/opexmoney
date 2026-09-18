bd0fb7679472b46e80467aabfc9839151cc0825f
Sajjad1276 <sajjad.1390125@gmail.com>
2026-09-18 20:46:33 +0330

    phase4: restore nation explore keyboard button import

--- app/handlers/nation.py
+++ app/handlers/nation.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from aiogram import F, Router
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, InlineKeyboardButton, Message
 from sqlalchemy import select
 
 from app.database.models import Nation, User
