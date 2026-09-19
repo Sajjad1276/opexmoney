4202be90a13b73f70ae131ba73e59cf11825f241
Sajjad1276 <sajjad.1390125@gmail.com>
2026-09-19 18:40:22 +0330

    feat: add price alert FSM state

--- app/states/market.py
+++ app/states/market.py
@@ -4,3 +4,4 @@ from aiogram.fsm.state import State, StatesGroup
 class MarketStates(StatesGroup):
     WAITING_BUY_AMOUNT = State()
     WAITING_SELL_AMOUNT = State()
+    WAITING_ALERT_PRICE = State()
