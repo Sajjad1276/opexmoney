3768f37113d5fad0be562cbef96964778c68d940
Sajjad1276 <sajjad.1390125@gmail.com>
2026-09-19 08:42:28 +0330

    Add ONBOARDING_NAME state

--- app/states/onboarding.py
+++ app/states/onboarding.py
@@ -2,5 +2,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class OnboardingStates(StatesGroup):
+    ONBOARDING_NAME = State()
     SET_USERNAME_PLAYER = State()
     SELECT_NATION = State()
