eac947df38d86703afc8a8048f8a8fbc2a68352a
Sajjad1276 <sajjad.1390125@gmail.com>
2026-09-19 14:37:30 +0330

    feat: register settings router

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ from app.handlers.market import router as market_router
 from app.handlers.missions import router as missions_router
 from app.handlers.portfolio import router as portfolio_router
 from app.handlers.ranking import router as ranking_router
+from app.handlers.settings import router as settings_router
 from app.handlers.nation import nation_router
 from app.handlers.nation_management import (
     expire_join_requests,
@@ -288,6 +289,7 @@ async def main() -> None:
     dp.include_router(missions_router)
     dp.include_router(portfolio_router)
     dp.include_router(ranking_router)
+    dp.include_router(settings_router)
     dp.include_router(founder_router)
     dp.include_router(nation_management_router)
     dp.include_router(nation_router)
