7c2328c1debdf798755cd82b82358cb960826f45
Sajjad1276 <sajjad.1390125@gmail.com>
2026-09-20 06:47:49 +0330

    refactor(founder): simplify founding wizard states

--- app/states/founder.py
+++ app/states/founder.py
@@ -4,5 +4,5 @@ from aiogram.fsm.state import State, StatesGroup
 class FounderStates(StatesGroup):
     WAITING_GROUP_ADMIN = State()
     SET_NATION_NAME = State()
-    CONFIRM = State()
     SELECT_FLAG = State()
+    CONFIRM = State()
