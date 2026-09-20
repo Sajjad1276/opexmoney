b1a67b53a97ea1997422bb6fd504eb9448a2d7fc
Sajjad1276 <sajjad.1390125@gmail.com>
2026-09-20 18:48:09 +0330

    refactor: add market symbol selection states

--- app/states/market.py
+++ app/states/market.py
@@ -4,4 +4,4 @@ from aiogram.fsm.state import State, StatesGroup
 class MarketStates(StatesGroup):
     WAITING_BUY_AMOUNT = State()
     WAITING_SELL_AMOUNT = State()
-    WAITING_ALERT_PRICE = State()
+    WAITING_ALERT_CURRENCY = State()\n    WAITING_ALERT_PRICE = State()\n    WAITING_CHART_CURRENCY = State()
